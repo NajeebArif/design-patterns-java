4a4a79564d6b5fa4a7e15786e62a845ae16dfccc
Najeeb Arif <NajeebArif786@outlook.com>
2023-04-16 16:33:08 +0530

    Add code for template method with a hook

--- src/main/java/narif/poc/designpatterns/templatemethod/CaffeineBeverageApp.java
+++ src/main/java/narif/poc/designpatterns/templatemethod/CaffeineBeverageApp.java
@@ -8,6 +8,9 @@ public class CaffeineBeverageApp {
 
         final var coffee = new Coffee();
         coffee.prepareRecipe();
+
+        CaffeineBeverageWithHook caffeineBeverageWithHook = new CoffeeWithHook();
+        caffeineBeverageWithHook.prepareRecipe();
     }
 }
 
